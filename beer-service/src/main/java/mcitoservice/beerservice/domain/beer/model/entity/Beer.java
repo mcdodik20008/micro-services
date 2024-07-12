bf0520caa74d533084f13276efd1fa666f0c40eb
Каспшицкий Алексей <kaspshitskii@technocom.tech>
2024-07-12 14:22:28 +0500

    настрока работы в доккере

--- beer-service/src/main/java/mcitoservice/beerservice/domain/beer/model/entity/Beer.java
+++ beer-service/src/main/java/mcitoservice/beerservice/domain/beer/model/entity/Beer.java
@@ -1,4 +1,23 @@
 package mcitoservice.beerservice.domain.beer.model.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.aot.generate.Generated;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Setter
+@Table("beer")
 public class Beer {
+
+    @Id
+    @Column(value = "id")
+    private Long id = 0L;
+
+    private String name = "Чекушка темного не фильтрованного";
+
+    private String volume = "Пока хочется";
+
 }
