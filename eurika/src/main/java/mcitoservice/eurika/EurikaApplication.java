d8932a15e611b6dddb10ab52a7e1ec2673274857
Каспшицкий Алексей <kaspshitskii@technocom.tech>
2024-07-12 11:23:37 +0500

    Настройка еврики

--- eurika/src/main/java/mcitoservice/eurika/EurikaApplication.java
+++ eurika/src/main/java/mcitoservice/eurika/EurikaApplication.java
@@ -2,8 +2,10 @@ package mcitoservice.eurika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurikaApplication {
 
     public static void main(String[] args) {
