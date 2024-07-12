336db7aac57384270f9b7400194b15237a65ab8b
Каспшицкий Алексей <kaspshitskii@technocom.tech>
2024-07-12 11:21:51 +0500

    инит микросервисов

---
+++ eurika/src/main/java/mcitoservice/eurika/EurikaApplication.java
@@ -0,0 +1,13 @@
+package mcitoservice.eurika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurikaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurikaApplication.class, args);
+    }
+
+}
---
+++ eurika/src/test/java/mcitoservice/eurika/EurikaApplicationTests.java
@@ -0,0 +1,13 @@
+package mcitoservice.eurika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurikaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
