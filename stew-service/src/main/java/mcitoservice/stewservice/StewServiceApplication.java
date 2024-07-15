16a087bb7edd6386ea383f52be42e6e2cb20c7cb
Каспшицкий Алексей <kaspshitskii@technocom.tech>
2024-07-15 09:36:25 +0500

    Добавил кафку и сервис для производства кроликов, производства похлебки

---
+++ stew-service/src/main/java/mcitoservice/stewservice/StewServiceApplication.java
@@ -0,0 +1,13 @@
+package mcitoservice.stewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StewServiceApplication.class, args);
+    }
+
+}
---
+++ stew-service/src/test/java/mcitoservice/stewservice/StewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mcitoservice.stewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
